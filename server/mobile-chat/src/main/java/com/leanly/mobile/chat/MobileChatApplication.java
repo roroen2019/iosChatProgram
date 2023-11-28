e2b01b9b8256b333341002b4894e64f663518953
constant94-dev <constant94@naver.com>
2023-11-28 17:24:46 +0900

    feat(server): 서버 폴더 구조 변경

---
+++ server/mobile-chat/src/main/java/com/leanly/mobile/chat/MobileChatApplication.java
@@ -0,0 +1,13 @@
+package com.leanly.mobile.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileChatApplication.class, args);
+	}
+
+}
---
+++ server/mobile-chat/src/test/java/com/leanly/mobile/chat/MobileChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leanly.mobile.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
