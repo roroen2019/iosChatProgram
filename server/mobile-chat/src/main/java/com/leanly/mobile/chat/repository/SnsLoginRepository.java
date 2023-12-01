2a1d76244f79f2c0914cde7fe65bd4de705b2269
Aaron(constant94-dev) <constant94@naver.com>
2023-12-01 18:35:28 +0900

    feat(repository): 유저, SNS 로그인 데이터베이스 통신을 위한 repo 생성

---
+++ server/mobile-chat/src/main/java/com/leanly/mobile/chat/repository/SnsLoginRepository.java
@@ -0,0 +1,8 @@
+package com.leanly.mobile.chat.repository;
+
+import com.leanly.mobile.chat.model.SnsLogin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SnsLoginRepository extends CrudRepository<SnsLogin, Long> {
+
+}
