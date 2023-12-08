42e4eb2f210211fc3cc98733e44ad8f2355d5277
Aaron(constant94-dev) <constant94@naver.com>
2023-12-08 17:18:03 +0900

    feat(service): 친구 관련 데이터 처리 메서드 구현

--- server/mobile-chat/src/main/java/com/leanly/mobile/chat/service/FriendService.java
+++ server/mobile-chat/src/main/java/com/leanly/mobile/chat/service/FriendService.java
@@ -1,11 +1,13 @@
 package com.leanly.mobile.chat.service;
 
+import com.leanly.mobile.chat.model.dto.FriendAddDTO;
+import com.leanly.mobile.chat.model.dto.FriendDTO;
 import com.leanly.mobile.chat.model.entity.Friend;
 
 public interface FriendService {
 
     void saveFromMemberId(Friend fromMemberId);
 
-    void addMemberFriends(Friend friend);
+    FriendDTO addFriendToMember(FriendAddDTO addDTO);
 
 }
