475d40b50368bd3b10161ee0a9b13b22bb7d7d3b
Aaron(constant94-dev) <constant94@naver.com>
2023-12-08 17:17:12 +0900

    feat(repository): 친구 JPA 저장소 생성

--- server/mobile-chat/src/main/java/com/leanly/mobile/chat/repository/FriendRepository.java
+++ server/mobile-chat/src/main/java/com/leanly/mobile/chat/repository/FriendRepository.java
@@ -2,7 +2,11 @@ package com.leanly.mobile.chat.repository;
 
 import com.leanly.mobile.chat.model.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
+    @Query(value = "select f from Friend f where f.fromMemberId = :fromMemberId")
+    Friend findFromMember(@Param("fromMemberId") Long fromMemberId);
 }
