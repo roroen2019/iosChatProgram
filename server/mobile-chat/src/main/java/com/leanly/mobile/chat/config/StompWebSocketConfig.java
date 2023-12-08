1c245ef522d6fc388387e631f8d010ccc8fff64c
Aaron(constant94-dev) <constant94@naver.com>
2023-12-08 17:10:42 +0900

    feat(config): STOMP 소켓 통신 설정 수정

--- server/mobile-chat/src/main/java/com/leanly/mobile/chat/config/StompWebSocketConfig.java
+++ server/mobile-chat/src/main/java/com/leanly/mobile/chat/config/StompWebSocketConfig.java
@@ -17,23 +17,18 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
             .addEndpoint("/stomp/chat")
-            .setAllowedOrigins("http://localhost:8888")
+            //sockJs 클라이언트가 websocket handshake로 커넥션할 경로
+            .setAllowedOrigins("http://*:*")
+            //가능한 경로 설정 ( 전체 오픈 : 기호에따라 수정 )
             .withSockJS();
     }
 
     /* 애플리케이션 내부에서 사용할 path를 지정할 수 있다 */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        /*
-         * Client에서 SEND 요청을 처리
-         * spring docs 에서는 /app 으로 예시
-         * */
         config.setApplicationDestinationPrefixes("/pub");
-        /*
-         * 해당 경로로 SimpleBroker를 등록
-         * SimpleBroker는 해당하는 경로를 SUBSCRIBE 하는 Client에게 메시지를 전달하는 간단한 작업을 수행
-         * spring docs 에서는 /topic , /queue 로 예시
-         * */
+        // /pub 으로 시작하는 stomp 메세지의 경로는 @controller @MessageMaping 메서드로 라우팅
         config.enableSimpleBroker("/sub");
+        // /sub 로 시작하는 stomp 메세지는 브로커로 라우팅함
     }
 }
